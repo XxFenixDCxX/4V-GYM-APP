26686af9c994c5bca57d9c9f4846ab791b7f0812
Arkaitz Calvo Saldias <acalvosald@educacion.navarra.es>
2023-12-12 09:51:05 +0100

    Agregado gets

--- src/app/utils/global-service.service.ts
+++ src/app/utils/global-service.service.ts
@@ -1,10 +1,17 @@
 import { Injectable } from '@angular/core';
 import { LoggerServiceService } from './logger-service.service';
 export interface Actividad {
+  id: number;
   fecha: string;
   lugar: string;
-  monitor: string;
+  monitor: Monitor[];
   tipo: TipoActividad;
+}
+export interface Monitor{
+  id: number;
+  nombre: string;
+  email: string;
+  telefono: string;
 }
 export enum TipoActividad {
   BodyPump = "BodyPump",
@@ -16,33 +23,58 @@ export enum TipoActividad {
 })
 export class GlobalServiceService {
   actividades: Actividad[];
+  monitores: Monitor[];
   constructor(private logger: LoggerServiceService) {
-    this.actividades = [];
-  }
-  getActivitys(){
-    this.logger.log("Recibiendo actividades");
+    this.monitores = [
+      {
+        id: 0,
+        nombre: "Juan",
+        email: "juan@gmail.com",
+        telefono: "948128888"
+      },
+      {
+        id: 1,
+        nombre: "Maria",
+        email: "juan@gmail.com",
+        telefono: "948128888"
+      },
+      {
+        id: 2,
+        nombre: "Pedro",
+        email: "juan@gmail.com",
+        telefono: "948128888"
+      }
+    ]
     this.actividades = [
       {
+        id: 0,
         fecha: "2021-10-01",
         lugar: "Gimnasio A",
-        monitor: "Juan",
+        monitor: [this.monitores[0]],
         tipo: TipoActividad.BodyPump
       },
       {
+        id: 1,
         fecha: "2021-10-02",
         lugar: "Gimnasio B",
-        monitor: "Maria",
+        monitor: [this.monitores[1]],
         tipo: TipoActividad.Pilates
       },
       {
+        id: 2,
         fecha: "2021-10-03",
         lugar: "Gimnasio C",
-        monitor: "Pedro",
+        monitor: [this.monitores[1], this.monitores[2]],
         tipo: TipoActividad.Spinning
       }
-    ];
-
+    ];;
+  }
+  getActivitys(){
+    this.logger.log("Recibiendo actividades");
     return this.actividades;
   }
-
+  getMonitors(){
+    this.logger.log("Recibiendo monitores");
+    return this.monitores;
+  }
 }
