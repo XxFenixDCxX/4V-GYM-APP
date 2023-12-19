45a50685ff2529d241adff0d5e949fe81acc0f91
XxFenixDCxX <fenixdc7@gmail.com>
2023-12-19 16:53:07 +0100

    Tab Footer

---
+++ src/app/footer-tabs/footer-tabs.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer-tabs',
+  standalone: true,
+  imports: [],
+  templateUrl: './footer-tabs.component.html',
+  styleUrl: './footer-tabs.component.scss'
+})
+export class FooterTabsComponent {
+
+}
