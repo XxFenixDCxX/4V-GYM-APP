dab6fa36a59dbd0cfac6a2f6b06fd9e3c9dc85d4
Arkaitz Calvo Saldias <fenixdc7@gmail.com>
2023-12-26 15:37:07 +0100

    Agragada funcion de agregar y editar

--- src/app/monitors/monitors.component.ts
+++ src/app/monitors/monitors.component.ts
@@ -1,22 +1,54 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { GlobalServiceService, Monitor } from '../utils/global-service.service';
 import { CarouselModule } from 'primeng/carousel';
 @Component({
   selector: 'app-monitors',
   standalone: true,
-  imports: [CommonModule, CarouselModule],
+  imports: [CommonModule, CarouselModule, FormsModule],
   templateUrl: './monitors.component.html',
   styleUrl: './monitors.component.scss'
 })
 export class MonitorsComponent implements OnInit {
   monitors: Monitor[] = [];
   numVisible: number = 3;
+  newMonitor: Monitor = { nombre: '', email: '', telefono: '' };
+  isModalOpen: boolean = false;
   constructor(private globalService: GlobalServiceService) {}
   ngOnInit(): void {
     this.monitors = this.globalService.getMonitors();
   }
 
+  openModal(): void {
+    this.isModalOpen = true;
+  }
+
+  closeModal(): void {
+    this.isModalOpen = false;
+    this.newMonitor = { nombre: '', email: '', telefono: '' };
+  }
+  loadMonitor(monitor: Monitor): void {
+    // Asigna los valores del monitor al modelo
+    this.newMonitor = { ...monitor };
+    // Abre el modal
+    this.openModal();
+  }    
+  addMonitor(): void {
+    const existingMonitor = this.monitors.find(m => m.id === this.newMonitor.id);
+  
+    if (existingMonitor) {
+      // Actualiza el monitor existente
+      Object.assign(existingMonitor, this.newMonitor);
+    } else {
+      // Agrega un nuevo monitor
+      this.globalService.addMonitor(this.newMonitor);
+    }
+  
+    // Restablece el formulario y cierra el modal
+    this.newMonitor = { nombre: '', email: '', telefono: '' };
+    this.closeModal();
+  }  
   removeMonitor(monitorId: number | undefined): void {
     if (monitorId !== undefined) {
       // Llama al método removeMonitor del servicio GlobalServiceService
