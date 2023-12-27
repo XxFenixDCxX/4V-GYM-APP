8d65a48fc8802f370f1b4f6aa988964fe32d3f80
Arkaitz Calvo Saldias <fenixdc7@gmail.com>
2023-12-27 14:18:20 +0100

    Agregadas funciones para mostrar siguiente o anterior actividad y para mostrar titulo de fecha seleccionada

--- src/app/activitys/activitys.component.ts
+++ src/app/activitys/activitys.component.ts
@@ -11,5 +11,20 @@ import {MatNativeDateModule} from '@angular/material/core';
   styleUrls: ['./activitys.component.scss']
 })
 export class ActivitysComponent {
-  date: Date | null = new Date();
+  date: Date = new Date();
+  stringContenido2: string = 'Ninguno Seleccionado';
+  onDateChange(): void {
+    // Actualizar el texto cuando cambie la fecha seleccionada
+    const options: Intl.DateTimeFormatOptions = { month: 'long' };
+    const month = new Intl.DateTimeFormat('es-ES', options).format(this.date);
+    this.stringContenido2 = this.date.getDate() + ' ' + month.charAt(0).toUpperCase() + month.slice(1) + " " + this.date.getFullYear();
+  }
+  nextDay(): void {
+    // Incrementar la fecha en 1 día
+    this.date.setDate(this.date.getDate() + 1);
+  }
+  prevDay(): void {
+    // Decrementar la fecha en 1 día
+    this.date.setDate(this.date.getDate() - 1);
+  }
 }
