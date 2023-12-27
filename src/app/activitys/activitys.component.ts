e494f4d162730997c040fec161b2e19b31349e60
Arkaitz Calvo Saldias <fenixdc7@gmail.com>
2023-12-27 14:56:56 +0100

    Agregados apartados para las actividades

--- src/app/activitys/activitys.component.ts
+++ src/app/activitys/activitys.component.ts
@@ -3,17 +3,22 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatCardModule } from '@angular/material/card';
 import { NgClass } from '@angular/common';
 import { MatNativeDateModule } from '@angular/material/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-activitys',
   standalone: true,
-  imports: [MatCardModule, MatDatepickerModule, MatNativeDateModule, NgClass],
+  imports: [MatCardModule, MatDatepickerModule, MatNativeDateModule, NgClass, CommonModule],
   templateUrl: './activitys.component.html',
   styleUrls: ['./activitys.component.scss']
 })
 export class ActivitysComponent {
   date: Date = new Date();
   stringContenido2: string = 'Ninguno Seleccionado';
+  cont1: boolean = false;
+  cont2: boolean = false;
+  cont3: boolean = false;
+
   onDateChange(): void {
     // Actualizar el texto cuando cambie la fecha seleccionada
     const options: Intl.DateTimeFormatOptions = { month: 'long' };
